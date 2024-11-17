b306d52e95bd616cad2a519f3c8d28247d5c4b11
umeshchandra15 <umeshchandrekanti1234@gmail.com>
2024-11-17 22:36:26 +0530

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+console.log(saveEl);
+function increment(){
+   count += 1;
+   countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+countEl.textcountent = 0;
+    count = 0;
+
+}
+
+let age =21;
+if( age => 21){
+    console.log("you can't enter rhe club");
+}
+else{
+    console.log("welcome!");
+}
